ddd1663516a96603dc146baf660d5ecd7d98c7ce
Gábor Szilágyi <szilagyi.gabor.krisztian2@outlook.com>
2022-02-25 15:02:37 +0100

    background color set in styles.css

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -16,27 +16,46 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     this.items = [
       {
-          label: 'File',
-          items: [{
-                  label: 'New', 
-                  icon: 'pi pi-fw pi-plus',
-                  items: [
-                      {label: 'Project'},
-                      {label: 'Other'},
-                  ]
-              },
-              {label: 'Open'},
-              {label: 'Quit'}
-          ]
+        label: 'My Todo List App'
       },
       {
-          label: 'Edit',
-          icon: 'pi pi-fw pi-pencil',
-          items: [
-              {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-              {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-          ]
+        label: 'TODO Searchbar here'
+      },
+      {
+        label: 'Color themes',
+        items: [
+          { label: 'Dark' },
+          { label: 'Light' },
+          { label: 'TODO accessibility'}
+        ]
+      },
+      {
+        label: 'Settings TODO icon'
+      },      {
+        label: 'Logout TODO icon'
       }
+      // {
+      //     label: 'File',
+      //     items: [{
+      //             label: 'New', 
+      //             icon: 'pi pi-fw pi-plus',
+      //             items: [
+      //                 {label: 'Project'},
+      //                 {label: 'Other'},
+      //             ]
+      //         },
+      //         {label: 'Open'},
+      //         {label: 'Quit'}
+      //     ]
+      // },
+      // {
+      //     label: 'Edit',
+      //     icon: 'pi pi-fw pi-pencil',
+      //     items: [
+      //         {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+      //         {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+      //     ]
+      // }
   ];
   }
 
